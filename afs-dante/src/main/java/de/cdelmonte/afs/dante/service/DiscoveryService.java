9c520e8c07f7132b36ea79645ca24399ebadf87d
Christian Del Monte <c.monte@qipu.de>
2018-05-04 18:06:07 +0200

    Clean pom

--- afs-dante/src/main/java/de/cdelmonte/afs/dante/service/DiscoveryService.java
+++ afs-dante/src/main/java/de/cdelmonte/afs/dante/service/DiscoveryService.java
@@ -1,30 +1,29 @@
 package de.cdelmonte.afs.dante.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class DiscoveryService {
-	@Autowired
-	RestTemplate restTemplate;
+  @Autowired
+  RestTemplate restTemplate;
 
-	@Autowired
-	private DiscoveryClient discoveryClient;
+  @Autowired
+  private DiscoveryClient discoveryClient;
 
-	public List getEurekaServices() {
-		List<String> services = new ArrayList<String>();
+  public List<String> getEurekaServices() {
+    List<String> services = new ArrayList<String>();
 
-		discoveryClient.getServices().forEach(serviceName -> {
-			discoveryClient.getInstances(serviceName).forEach(instance -> {
-				services.add(String.format("%s:%s", serviceName, instance.getUri()));
-			});
-		});
+    discoveryClient.getServices().forEach(serviceName -> {
+      discoveryClient.getInstances(serviceName).forEach(instance -> {
+        services.add(String.format("%s:%s", serviceName, instance.getUri()));
+      });
+    });
 
-		return services;
-	}
+    return services;
+  }
 }
